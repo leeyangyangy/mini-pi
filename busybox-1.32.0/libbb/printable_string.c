7d5cd4f5eb10d6aadc655010afdc77dd9801dd35
leeyangyangy <1787294587@qq.com>
2024-07-29 19:04:45 +0800

    support chinese

--- busybox-1.32.0/libbb/printable_string.c
+++ busybox-1.32.0/libbb/printable_string.c
@@ -15,11 +15,14 @@ const char* FAST_FUNC printable_string2(uni_stat_t *stats, const char *str)
 	const char *s;
 
 	s = str;
-	while (1) {
+	while (1)
+	{
 		unsigned char c = *s;
-		if (c == '\0') {
+		if (c == '\0')
+		{
 			/* 99+% of inputs do not need conversion */
-			if (stats) {
+			if (stats)
+			{
 				stats->byte_count = (s - str);
 				stats->unicode_count = (s - str);
 				stats->unicode_width = (s - str);
@@ -32,28 +35,6 @@ const char* FAST_FUNC printable_string2(uni_stat_t *stats, const char *str)
 			break;
 		s++;
 	}
-
-#if ENABLE_UNICODE_SUPPORT
-	dst = unicode_conv_to_printable(stats, str);
-#else
-	{
-		char *d = dst = xstrdup(str);
-		while (1) {
-			unsigned char c = *d;
-			if (c == '\0')
-				break;
-			if (c < ' ' || c >= 0x7f)
-				*d = '?';
-			d++;
-		}
-		if (stats) {
-			stats->byte_count = (d - dst);
-			stats->unicode_count = (d - dst);
-			stats->unicode_width = (d - dst);
-		}
-	}
-#endif
-	return auto_string(dst);
 }
 
 const char* FAST_FUNC printable_string(const char *str)
